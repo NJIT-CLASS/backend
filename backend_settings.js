897bc94aadd9fdf7a14abd8a3060f4f856327e01
Alan Romano <alan.rom25.95@gmail.com>
2018-02-18 18:07:34 -0500

    Fix email bug

--- backend_settings.js
+++ backend_settings.js
@@ -7,5 +7,4 @@ exports.SERVER_PORT = process.env.serverPort || '4000';
 exports.TOKEN_KEY = process.env.tokenSecret || 'secretKey';
 exports.TOKEN_LIFE = process.env.tokenLife || '1h';
 exports.REFRESH_TOKEN_LIFE = process.env.refreshTokenLife || 14;
-exports.EMAIL_PASSWORD = 'plamaster123';
-exports.SERVER_EMAIL = 'njitplamaster@gmail.com';
+
