0a4d853e79f5a834f64419a9459896d48e5fe685
Alan Romano <alan.rom25.95@gmail.com>
2018-03-23 05:50:55 -0400

    Fix lowercase table names

--- Model/UserBadges.js
+++ Model/UserBadges.js
@@ -63,6 +63,6 @@ module.exports = function(sequelize, DataTypes) {
         freezeTableName: true,
 
         // define the table's name
-        tableName: 'UserBadges'
+        tableName: 'userbadges'
     });
 };
