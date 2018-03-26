f2fbf52f902656d676aa9ff04fa062b35c261efd
caton <bcaton85@gmail.com>
2018-03-26 13:29:29 -0400

    Removed extra Role field in User model

--- Model/User.js
+++ Model/User.js
@@ -52,12 +52,12 @@ module.exports = function(sequelize, DataTypes) {
         //     allowNull: false,
         //     defaultValue: false
         // },
-        Role: {
+        //Role: {
             //Official first name of the user
-            type: DataTypes.STRING(40),
-            field: 'Role',
-            allowNull: true
-        }
+        //    type: DataTypes.STRING(40),
+        //    field: 'Role',
+         //   allowNull: true
+        //}
         //UserType: {
         //    //User type either instructor or student
         //    //Instructor Boolean?????
