7f53d0d40097eb8f8c620a80d28b628ca708bbdf
Alan Romano <alan.rom25.95@gmail.com>
2018-04-11 10:04:41 -0400

    Fix conflict in backend_settings

--- backend_settings.js
+++ backend_settings.js
@@ -1,21 +1,11 @@
 exports.DB_HOST = process.env.dbHost || 'localhost';
 exports.DB_USER = process.env.dbUser || 'root';
 exports.DB_PORT = process.env.dbPort || '3306';
-<<<<<<< HEAD
-exports.DB_PASS = process.env.dbPass || 'password';
-=======
 exports.DB_PASS = process.env.dbPass || '1234';
->>>>>>> 41bf26fe9d20859b980a16c83a1351f9b35dc159
 exports.DATABASE = process.env.database || 'class/pla';
 exports.SERVER_PORT = process.env.serverPort || '4000';
 exports.FRONT_SERVER_PORT ='4001';
 exports.TOKEN_KEY = process.env.tokenSecret || 'secretKey';
 exports.TOKEN_LIFE = process.env.tokenLife || '6h';
 exports.REFRESH_TOKEN_LIFE = process.env.refreshTokenLife || 14;
-<<<<<<< HEAD
 
-
-=======
-exports.EMAIL_PASSWORD = 'plamaster123';
-exports.SERVER_EMAIL = 'njitplamaster@gmail.com';
->>>>>>> 41bf26fe9d20859b980a16c83a1351f9b35dc159
