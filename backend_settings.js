0c88494f514ac340b8245900d9be7e2dd072085b
Alan Romano <alan.rom25.95@gmail.com>
2018-05-17 21:26:42 -0400

    Update SPs

--- backend_settings.js
+++ backend_settings.js
@@ -1,7 +1,7 @@
 exports.DB_HOST = process.env.dbHost || 'localhost';
 exports.DB_USER = process.env.dbUser || 'root';
 exports.DB_PORT = process.env.dbPort || '3306';
-exports.DB_PASS = process.env.dbPass || 'yamaha6163';
+exports.DB_PASS = process.env.dbPass || 'password';
 exports.DATABASE = process.env.database || 'class/pla';
 exports.SERVER_PORT = process.env.serverPort || '4000';
 exports.FRONT_SERVER_PORT ='4001';
